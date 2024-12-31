2adcc00a80ff4be71a1cb91286e959df1fd0e169
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2024-12-31 08:09:11 +0000

    Correct tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -76,9 +76,9 @@ class test_basemodel(unittest.TestCase):
 
     def test_kwargs_one(self):
         """ """
-        n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
+        n = {'name': 'test'}
+        new = self.value(**n)
+        self.assertEqual(n['name'], new.name)
 
     def test_id(self):
         """ """
