3608ff73aaf702898f055bde385ab2df689e7fb9
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-03 16:34:45 +0000

    Modify do_create method in console.py

--- console.py
+++ console.py
@@ -131,7 +131,7 @@ class HBNBCommand(cmd.Cmd):
                 setattr(new_instance, key, value)
         else:
             new_instance = HBNBCommand.classes[class_name]()
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
         storage.save()
 
