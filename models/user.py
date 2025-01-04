3d47cc1ab3ed7a7e404e419d55b34b2b0f1c956e
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-04 11:19:50 +0000

    Add updated models/user.py

--- models/user.py
+++ models/user.py
@@ -3,31 +3,40 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-
     __tablename__ = "users"
+    email = Column(
+            String(128),
+            nullable=False
+            )
 
-    email = Column(String(128),
-                   nullable=False
-                   )
+    password = Column(
+            String(128),
+            nullable=False
+            )
 
-    password = Column(String(128),
-                      nullable=False
-                      )
+    first_name = Column(
+            String(128),
+            nullable=True
+            )
 
-    first_name = Column(String(128))
+    last_name = Column(
+            String(128),
+            nullable=True
+            )
 
-    last_name = Column(String(128))
+    places = relationship(
+            "Place",
+            backref="user",
+            cascade="all, delete, delete-orphan"
+            )
 
-    places = relationship("Place",
-                          backref="user",
-                          cascade="all, delete, delete-orphan"
-                          )
-
-    reviews = relationship("Review",
-                           backref="user",
-                           cascade="all, delete, delete-orphan"
-                           )
+    reviews = relationship(
+            "Review",
+            backref="user",
+            cascade="all, delete, delete-orphan"
+            )
