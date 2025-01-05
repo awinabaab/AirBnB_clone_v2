ef33d0657aee9645e658b3976013b211b56cf056
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-05 14:43:24 +0000

    Modify __init__ method in models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -35,11 +35,19 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if 'id' not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs.keys():
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs.keys():
+                self.updated_at = datetime.now()
+            for key, value in kwargs.items():
+                if key == 'updated_at':
+                    kwargs['updated_at'] = datetime.fromisoformat(value)
+                if key == 'created_at':
+                    kwargs['created_at'] = datetime.fromisoformat(value)
+            if '__class__' in kwargs.keys():
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
