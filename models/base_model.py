590b86c72e72aa0fccbfa4acfa7e69f6ae8902d4
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-02-08 16:25:09 +0000

    Conditionally update the updated_at attribute to the current datetime

--- models/base_model.py
+++ models/base_model.py
@@ -60,7 +60,8 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        if 'update_at' in self.__dict__:
+            self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
